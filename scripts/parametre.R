59ec5a012a2d075227bea3d2990646c536dec76f
Eivind Hageberg <eivind.hageberg@gmail.com>
2023-03-03 14:43:50 +0100

    oppdateringer til script
    noen oppdateringer av script og enkelte analyser pr 2022

---
+++ scripts/parametre.R
@@ -0,0 +1,17 @@
+#parametre
+
+kunder = c(
+  "Arbeids-+og+inkluderingsdepartementet",
+  "NAV",
+  "%22Barne-,+ungdoms-+og+familiedirektoratet%22",
+  "Digitaliseringsdirektoratet",
+  "Direktoratet+for+forvaltning+og+økonomistyring+(DFØ)",
+  "%22Direktoratet+for+høyere+utdanning+og+kompetanse+(HK-dir)%22",
+  "HK-dir",
+  "Distriktssenteret",
+  "Integrerings-+og+mangfoldsdirektoratet+(IMDi)",
+  "Husbanken",
+  "Norad",
+  "Utdanningsdirektoratet",
+  "KS+(Kommunesektorens+organisasjon)"
+)
